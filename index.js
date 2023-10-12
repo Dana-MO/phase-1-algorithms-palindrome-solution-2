19fc3ab77b083fefead5f4ce16cea7aa0df5be6a
danaomar <danaomaark@gmail.com>
2023-10-12 09:29:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+/* 
+if the first letter is the same as the last letter, and the second letter is the same as the second to last letter, etc. until we reach the middle, return true.
+*/
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    //if any letters don't match, return false
+    if (startChar !== endChar) return false;
+
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+
+  iterate from the beginning to the middle
+    check each letter to the corresponding letter from the end
+      if any letters don't mathc, return false
+    
+  return true
 */
 
 /*
@@ -20,6 +42,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 }
 
 module.exports = isPalindrome;
